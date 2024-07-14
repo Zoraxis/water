9b6e8084836c42ca8af6b296600218bfa736ed37
amsalat <babashkanan@gmail.com>
2024-07-14 09:50:02 +0200

    markus

---
+++ canvas/simple/order/order.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var text = "PENIS"
+var currSymbol = 0
+
+var textDelay = 0
+var textmaxDelay = 1
+
+func setNewText(newText):
+	text = newText
+	currSymbol = 0
+
+func _process(delta):
+	if textDelay <= 0 and currSymbol < text.length():
+		$text.text += text[currSymbol]
+		currSymbol += 1
+		textDelay = textmaxDelay
+	
+	textDelay -= delta * 8
